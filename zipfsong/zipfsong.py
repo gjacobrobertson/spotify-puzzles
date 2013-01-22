39c61f2118f0d860360df0c654ae0149dacce29a
Jacob Robertson <g.jacob.robertson@gmail.com>
2013-01-22 15:44:28 -0500

    Improved zipfsong code readability

--- zipfsong/zipfsong.py
+++ zipfsong/zipfsong.py
@@ -14,11 +14,11 @@ if __name__ == '__main__':
     header = sys.stdin.readline()
     n, m = [int(val) for val in header.split(' ')]
     songs = []
-    for i in range(n):
+    for i in range(1, n + 1):
         line = sys.stdin.readline()
         vals = line.split(' ')
         plays = int(vals[0])
         title = vals[1].rstrip()
-        songs.append({'quality': plays * (i + 1), 'title': title})
+        songs.append({'quality': plays * i, 'title': title})
     for song in best_m_songs(m, songs):
         print song['title']
