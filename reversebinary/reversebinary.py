8296fb873889e8ef05809508527a4573c7ce1401
Jacob Robertson <g.jacob.robertson@gmail.com>
2013-01-22 15:40:48 -0500

    pep8 compliance

--- reversebinary/reversebinary.py
+++ reversebinary/reversebinary.py
@@ -1,14 +1,15 @@
 import sys
 
+
 def reverse_num(num):
-  rnum = num & 1
-  while num >> 1:
-    rnum <<= 1
-    num >>= 1
-    rnum |= (num & 1)
-  return rnum
+    rnum = num & 1
+    while num >> 1:
+        rnum <<= 1
+        num >>= 1
+        rnum |= (num & 1)
+    return rnum
 
 if __name__ == '__main__':
-  line =  sys.stdin.readline()
-  num = int(line)
-  print reverse_num(num)
+    line = sys.stdin.readline()
+    num = int(line)
+    print reverse_num(num)
--- zipfsong/zipfsong.py
+++ zipfsong/zipfsong.py
@@ -1,21 +1,24 @@
 import sys
 import heapq
 
+
 def key(song):
-  return song['quality']
+    return song['quality']
+
 
 def best_m_songs(m, songs):
-  return heapq.nlargest(m, songs, key)
+    return heapq.nlargest(m, songs, key)
+
 
 if __name__ == '__main__':
-  header = sys.stdin.readline()
-  n, m = [int(val) for val in header.split(' ')]
-  songs = []
-  for i in range(n):
-    line = sys.stdin.readline()
-    vals = line.split(' ')
-    plays = int(vals[0])
-    title = vals[1].rstrip()
-    songs.append({'quality': plays * (i+1), 'title': title})
-  for song in best_m_songs(m, songs):
-    print song['title']
+    header = sys.stdin.readline()
+    n, m = [int(val) for val in header.split(' ')]
+    songs = []
+    for i in range(n):
+        line = sys.stdin.readline()
+        vals = line.split(' ')
+        plays = int(vals[0])
+        title = vals[1].rstrip()
+        songs.append({'quality': plays * (i + 1), 'title': title})
+    for song in best_m_songs(m, songs):
+        print song['title']
